3cc1cf2e1dd549aab1ed3e5c158324b2643e23dd
marc-cr1810 <marc.cookerussell@gmail.com>
2021-12-09 23:27:16 +1100

    Update pacman after incluing Chaotic AUR

--- setup.sh
+++ setup.sh
@@ -39,11 +39,12 @@ sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 pacman -Sy --noconfirm
 
 # Setup Chaotic AUR
-pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
-pacman-key --lsign-key FBA220DFC880C036
+pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com --noconfirm
+pacman-key --lsign-key FBA220DFC880C036 --noconfirm
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 echo '[chaotic-aur]' >> "/etc/pacman.conf"
 echo 'Include = /etc/pacman.d/chaotic-mirrorlist' >> "/etc/pacman.conf"
+pacman -Sy --noconfirm
 
 echo -e "\nInstalling Base System\n"
 
